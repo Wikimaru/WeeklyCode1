085c5543bd823a1c42f0b2fc3d898683741630fa
Wikimaru <71822721+Wikimaru@users.noreply.github.com>
2021-09-06 18:45:57 +0700

    Add files via upload

---
+++ week5progarm1.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int x;
+	scanf("%d", &x);
+	for(int i=1;i<=x;i++)
+	{
+		for(int j=1;j<=(i+x)-1;j++)
+		{
+			if (j>x-i) 
+			{
+				printf("*");
+			}
+			else if((1+(2*(i-1)))<j)
+			{
+				printf(" ");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
